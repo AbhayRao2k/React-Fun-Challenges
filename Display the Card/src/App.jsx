7743fbbe7ea51fab1844f1930d238af95df23872
Abhay <abhayrao2k@gmail.com>
2025-03-04 20:56:47 +0530

    first commit

---
+++ Display the Card/src/App.jsx
@@ -0,0 +1,13 @@
+import Fetch from "./Fetch";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Fetch />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Display the Card/src/Fetch.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+const Fetch = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>
+        {posts.map((post) => {
+          return (
+            <div key={post.id}>
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Fetch;
